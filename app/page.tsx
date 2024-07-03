a4b5cc7700c80ddee249824c92c4424ee727f46f
lhl <harry.li@malipo-fintech.com>
2024-07-03 11:16:08 +0800

    feat: init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Hello } from "../pages/hello";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Hello></Hello>
+      </div>
+    </>
+  );
+}
